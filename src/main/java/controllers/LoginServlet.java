b90eab35cb572405c0e6c0653f421e496f6f81df
becca <r.alexis.tolentino@gmail.com>
2017-10-25 11:06:42 -0500

    controller Servlets created

---
+++ src/main/java/controllers/LoginServlet.java
@@ -0,0 +1,43 @@
+package src.main.java.controllers;
+
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import src.main.java.Dao.DaoFactory;
+import src.main.java.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF//login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user = DaoFactory.getUserDao().findByUsername(username);
+
+        if (user == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+
+        boolean validAttempt = password.equals(user.getpassword());
+
+        if (validAttempt) {
+            request.getSession().setAttribute("user", user);
+            response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
